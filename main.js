b2b6e3771a0d2a4a2731eb8c4dc505d64789c024
Dayarat <kavindusandaruwan703@gmail.com>
2023-08-13 22:12:48 +0530

    add gogle form

--- main.js
+++ main.js
@@ -159,3 +159,19 @@ window.onscroll = () => {
 //   }
 // })
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxQEdApm7tEhaFkcVQwlZjZv8N5oaKHPtkb0wHTEwRrI3iwtC8Akvezap5KsULzBhzP1A/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
